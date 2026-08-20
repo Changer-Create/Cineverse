c1c4f450964a8cc3ca46c56f35160d25b3028c7a
cj956151388-png <cj956151388@gmail.com>
2026-08-20 14:15:54 +0800

    feat: load navigation ordering modules

--- content-center.js
+++ content-center.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   const isAdminConsole=/(?:^|\/)admin-console\.html$/i.test(location.pathname);
-  document.write('<script src="site-brand.js"></script>');
+  document.write('<script src="site-brand.js"></script><script src="nav-order.js"></script>');
   if(isAdminConsole){
     document.documentElement.style.visibility='hidden';
     let token='';
@@ -10,5 +10,5 @@
     document.write('<script src="admin-auth.js"></script>');
   }
   document.write('<script src="content-observer-shield.js"></script><script src="content-center-core.js"></script><script src="content-compat.js"></script><script src="content-schema-extra.js"></script><script src="content-schema-guard.js"></script>');
-  if(isAdminConsole)document.write('<script src="admin-brand.js"></script>');
+  if(isAdminConsole)document.write('<script src="admin-brand.js"></script><script src="admin-nav.js"></script>');
 })();
